2a877bb27842c41a6dc27889ba3ee9ab4a7ef1a3
bravexist.cn <qiaoxiong9626@gmail.com>
2024-08-06 23:37:24 +0800

    1

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './permission'
-import router from './plugins/router'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import './app.css'
-import 'element-plus/dist/index.css'
-
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	content: ['./index.html', './src/**/*.vue'],
-	theme: {
-		extend: {}
-	},
-	plugins: [],
-	important: true
-}
